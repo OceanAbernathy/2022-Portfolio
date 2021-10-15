5db751bb7c087736a9941ce170e168d5e1b99579
Ocean Abernathy <ocean.abernathy@gmail.com>
2021-10-15 09:34:06 -0700

    PR Push

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,40 +1,33 @@
 import React from 'react';
-import { UilHome } from '@iconscout/react-unicons';
-import { UilUser } from '@iconscout/react-unicons';
-import { UilFileAlt } from '@iconscout/react-unicons';
-import { UilScenery } from '@iconscout/react-unicons';
-import { UilMessage } from '@iconscout/react-unicons';
-import { UilFileDownload } from '@iconscout/react-unicons';
-import { UilTimes } from '@iconscout/react-unicons';
-import { UilApps } from '@iconscout/react-unicons';
 
 function Header() {
 
-    const navMenu = document.getElementById('nav-menu'),
-          navToggle = document.getElementById('nav-toggle'),
-          navClose = document.getElementById('nav-close');
+    document.addEventListener("DOMContentLoaded", function(){
+        const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close');
+  
+        if(navToggle) {
+            navToggle.addEventListener('click', () => {
+                navMenu.classList.add('show-menu')
+            })
+        };
 
-    
-    if(navToggle) {
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('show-menu')
-        })
-    };
+        if(navClose) {
+            navClose.addEventListener('click', () => {
+                navMenu.classList.remove('show-menu')
+            })
+        };
 
-    if(navClose) {
-        navClose.addEventListener('click', () => {
+        const navLink = document.querySelectorAll('.nav__link')
+
+        function linkAction(){
+            const navMenu = document.getElementById('nav-menu')
+            // When we click on each nav__link, we remove the show-menu class
             navMenu.classList.remove('show-menu')
-        })
-    };
-
-    const navLink = document.querySelectorAll('.nav__link')
-
-    function linkAction(){
-        const navMenu = document.getElementById('nav-menu')
-        // When we click on each nav__link, we remove the show-menu class
-        navMenu.classList.remove('show-menu')
-    }
-    navLink.forEach(n => n.addEventListener('click', linkAction))
+        }
+        navLink.forEach(n => n.addEventListener('click', linkAction))
+    });
 
     return (
         <div className="header">
@@ -45,40 +38,40 @@ function Header() {
                     <ul className="nav__list grid">
                         <li className="nav__item">
                             <a href="#home" className="nav__link">
-                                <UilHome className="uil nav__icon" /> Home
+                            <i className="uil uil-home nav__icon"></i> Home
                             </a>
                         </li>
                         <li className="nav__item">
                             <a href="#about" className="nav__link">
-                                <UilUser className="uil nav__icon" /> About
+                            <i className="uil uil-user nav__icon"></i> About
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <UilFileAlt className="uil nav__icon" /> Skills
+                            <a href="#skills" className="nav__link">
+                            <i className="uil uil-file-alt nav__icon"></i> Skills
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <UilScenery className="uil nav__icon" /> Portfolio
+                            <a href="#portfolio" className="nav__link">
+                            <i className="uil uil-scenery nav__icon"></i> Portfolio
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <UilFileDownload className="uil nav__icon" /> Resume
+                            <a href="./Assets/OceanAbernathyResume.pdf" download className="nav__link">
+                            <i className="uil uil-file-download nav__icon"></i> Resume
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <UilMessage className="uil nav__icon" /> Contact
+                            <a href="#contact" className="nav__link">
+                            <i className="uil uil-message nav__icon"></i> Contact
                             </a>
                         </li>
                     </ul>
-                    <UilTimes className="uil close__icon" id="nav-close" />
+                    <i className="uil uil-times close__icon" id="nav-close"></i>
                 </div>
                 <div className="nav__buttons">
                     <div className="nav__toggle" id="nav-toggle">
-                        <UilApps className="uil toggle__icon" />
+                        <i className="uil uil-apps toggle__icon"></i>
                     </div>
                 </div>
             </nav>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,145 @@
 import React from 'react';
-import './Main.css';
 
 function Main() {
+
     return (
-        <div className="main__content">
-            <svg className="blob" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <mask id="mask0" mask-type="alpha">
-                    <path d="M42.5,-12.2C50.8,11.8,50.4,40.2,35,52.4C19.6,64.6,-10.7,60.5,-32.6,44.6C-54.5,28.7,-68.1,1.1,-61.3,-20.8C-54.4,-42.7,-27.2,-58.9,-5.1,-57.3C17.1,-55.6,34.2,-36.1,42.5,-12.2Z"  transform="translate(100 100)" />
-                </mask>
-                <g mask="url(#mask0)">
-                    <path d="M42.5,-12.2C50.8,11.8,50.4,40.2,35,52.4C19.6,64.6,-10.7,60.5,-32.6,44.6C-54.5,28.7,-68.1,1.1,-61.3,-20.8C-54.4,-42.7,-27.2,-58.9,-5.1,-57.3C17.1,-55.6,34.2,-36.1,42.5,-12.2Z" transform="translate(100 100)" />
-                    <image x="40" y="48" href="./grayscale-portrait-updated-removebg.png" />
-                </g>
-            </svg>
-        </div>
+        <main className="main__content">
+            <span className="overlay"></span>
+            <section className="home section" id="home">
+                <div className="home__container container grid">
+                    <div className="home__content grid">
+                        <div className="home__social">
+                            <a href="https://www.linkedin.com/in/ocean-abernathy/" target="_blank" className="home__social-icon">
+                                <i className="uil uil-linkedin-alt social-icon"></i>
+                            </a>
+                            <a href="https://github.com/OceanAbernathy" target="_blank" className="home__social-icon">
+                                <i className="uil uil-github-alt social-icon"></i>
+                            </a>
+                            <a href="mailto:ocean.abernathy@gmail.com" target="_blank" className="home__social-icon">
+                                <i className="uil uil-envelope social-icon"></i>
+                            </a>
+                        </div>
+                        <div className="home__img">
+                            <svg className="home__blob" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                                <mask id="mask0" mask-type="alpha">
+                                    <path d="M42.5,-12.2C50.8,11.8,50.4,40.2,35,52.4C19.6,64.6,-10.7,60.5,-32.6,44.6C-54.5,28.7,-68.1,1.1,-61.3,-20.8C-54.4,-42.7,-27.2,-58.9,-5.1,-57.3C17.1,-55.6,34.2,-36.1,42.5,-12.2Z"  transform="translate(100 100)" />
+                                </mask>
+                                <g mask="url(#mask0)">
+                                    <path d="M42.5,-12.2C50.8,11.8,50.4,40.2,35,52.4C19.6,64.6,-10.7,60.5,-32.6,44.6C-54.5,28.7,-68.1,1.1,-61.3,-20.8C-54.4,-42.7,-27.2,-58.9,-5.1,-57.3C17.1,-55.6,34.2,-36.1,42.5,-12.2Z" transform="translate(100 100)" />
+                                    <image className="home__blob-img" x="40" y="48" href="./grayscale-portrait-updated-removebg.png" />
+                                </g>
+                            </svg>
+                        </div>
+
+                        <div className="home__data">
+                            <h1 className="home__title">Hello, my name is Ocean.</h1>
+                            <h2 className="home__subtitle">Front-End Web Developer</h2>
+                            <p className="home__description"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti iste ex illum consectetur recusandae.</p>
+                            <a href="#contact" className="button button--flex contact__button">
+                                Contact Me <i className="uil uil-message button__icon"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="home__scroll">
+                        <a href="#about" className="home__scroll-button button--flex">
+                            <i className="uil uil-mouse-alt home__scroll-mouse"></i>
+                            <span className="home__scroll-name">Scroll down</span>
+                            <i className="uil uil-arrow-down home__scroll-arrow"></i>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <section className="about section" id="about">
+                <h2 className="section__title">About Me</h2>
+                <span className="section__subtitle">My introduction</span>
+
+                <div className="about__container container grid">
+                    <div className="about__data">
+                        <p className="about__description"></p>
+
+                        <div className="about__info">
+                            <div>
+                                <span className="about__info-title">08+</span>
+                                <span className="about__info-name">Years <br /> experience</span>
+                            </div>
+
+                            <div>
+                                <span className="about__info-title">20+</span>
+                                <span className="about__info-name">Completed <br /> project</span>
+                            </div>
+
+                            <div>
+                                <span className="about__info-title">05+</span>
+                                <span className="about__info-name">Companies <br /> worked</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="skills section" id="skills">
+                <h2 className="section__title">Skills</h2>
+                <span className="section__subtitle">My technical level</span>
+
+                <div className="skills__container container grid">
+                    <div>
+                        <div className="skills__content">
+                            <div className="skills__header">
+                            <i className="uil uil-brackets-curly skills__icon"></i>
+                                <div>
+                                    <h1 className="skills__title">Frontend developer</h1>
+                                    <span className="skills__subtitle">More than 4 years</span>
+                                </div>
+                                <i className="uil uil-angle-down skills__arrow"></i>
+                            </div>
+
+                            <div className="skills__list skills__open grid">
+                                <div className="skills__data">
+                                    <div className="skills__titles">
+                                        <h3 className="skills__name">HTML</h3>
+                                        <span className="skills__number">90%</span>
+                                    </div>
+                                    <div className="skills__bar">
+                                        <span className="skills__percentage skills__html"></span>
+                                    </div>
+                                </div>
+
+                                <div className="skills__data">
+                                    <div className="skills__titles">
+                                        <h3 className="skills__name">CSS</h3>
+                                        <span className="skills__number">80%</span>
+                                    </div>
+                                    <div className="skills__bar">
+                                        <span className="skills__percentage skills__css"></span>
+                                    </div>
+                                </div>
+
+                                <div className="skills__data">
+                                    <div className="skills__titles">
+                                        <h3 className="skills__name">JavaScript</h3>
+                                        <span className="skills__number">60%</span>
+                                    </div>
+                                    <div className="skills__bar">
+                                        <span className="skills__percentage skills__js"></span>
+                                    </div>
+                                </div>
+
+                                <div className="skills__data">
+                                    <div className="skills__titles">
+                                        <h3 className="skills__name">React</h3>
+                                        <span className="skills__number">40%</span>
+                                    </div>
+                                    <div className="skills__bar">
+                                        <span className="skills__percentage skills__react"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
 
